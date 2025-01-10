8cc73ace8356271edc2080a119770316b83dc397
Alice Ferreira <alice.hex@proton.me>
2025-01-10 13:19:48 -0300

    Primeflix Completo

--- primeflix/src/pages/Filme/index.js
+++ primeflix/src/pages/Filme/index.js
@@ -1,8 +1,82 @@
+import { useEffect,useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import './filme-info.css';
+import { toast } from "react-toastify";
+
+import api from '../../services/api'
 
 function Filme(){
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api.get(`/movie/${id}`, {
+        params:{
+          api_key: 'cfd7c0d84de2941640648444622bc787',
+          language: 'pt-BR',
+        }
+      })
+      .then((response) => {
+        setFilme(response.data);
+        setLoading(false);
+      })
+      .catch(()=>{
+        console.log("Filme não encontrado")
+        navigate("/")
+      })
+    }
+
+    loadFilme();
+
+    return () => {
+      console.log("Componente desmontado.")
+    }
+  }, [])
+
+  function salvarFilme(){
+    const minhaLista = localStorage.getItem("@primeflix")
+
+    let filmesSalvos = JSON.parse(minhaLista) || [];
+
+    const hasFilme = filmesSalvos.some( (filmesSalvo) => filmesSalvo.id === filme.id)
+
+    if(hasFilme){
+      toast.warn("Esse filme já está na sua lista.");
+      return;
+    }
+
+    filmesSalvos.push(filme);
+    localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+    toast.success("Filme salvo com sucesso!");
+  }
+
+  if(loading){
+    <div className="filme-info">
+      <h1>Carregando detalhes...</h1>
+    </div>
+  }
+
   return(
-    <div>
-      <h1>BEM VINDO A PAGINA DETALHES DO FILME</h1>
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}/>
+
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+
+      <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+      <div className="area-buttons">
+        <button onClick={salvarFilme}>Salvar</button>
+        <button>
+           <a target="_blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
+            Trailer
+           </a>
+        </button>
+      </div>
     </div>
   )
 }
--- primeflix/src/pages/Home/index.js
+++ primeflix/src/pages/Home/index.js
@@ -1,13 +1,13 @@
 import { useEffect, useState} from 'react';
 import api from '../../services/api';
 import { Link } from 'react-router-dom';
+import './home.css';
 
 // URL DA API: /movie/now_playing?api_key=cfd7c0d84de2941640648444622bc787&language=pt-BR
 
 function Home(){
   const [filmes, setFilmes] = useState([]);
-  const imgurl ='https://image.tmdb.org/t/p/original/';
-
+  const [loading, setLoading] = useState(true)
 
   useEffect(()=>{
 
@@ -22,23 +22,30 @@ function Home(){
 
       //console.log(response.data.results);
       setFilmes(response.data.results.slice(0,10))
-
+      setLoading(false)
     }
 
     loadFilmes();
 
   }, [])
 
+  if(loading){
+    return(
+      <div className='loading'>
+        <h2>Carregando filmes...</h2>
+      </div>
+    )
+  }
+
   return(
     <div className='container'>
       <div className='lista-filmes'>
         {filmes.map((filme) =>{
-          let imgconcat = imgurl+filme.poster_path;
           return(
             <article key={filme.id}>
               <strong>{filme.title}</strong>
-              <img src={imgconcat} alt={filme.title} />
-              <Link to={'/filme/${filme.id}'}>Acessar</Link>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
             </article>
           )
         })}
