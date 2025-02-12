0dcd44e9f74612ddd2e30a3cb1fdc3181e9d70b7
Alice Ferreira <alice.hex@proton.me>
2025-02-12 19:28:25 -0300

    Components
    https://www.youtube.com/watch?v=6Qqb2GBGgGc
    15:43

--- shopping-cart/src/App.tsx
+++ shopping-cart/src/App.tsx
@@ -1,13 +1,21 @@
+import Cart from "./components/Cart"
+import Header from "./components/Header"
+import Footer from "./components/footer"
+import ProductList from "./components/ProductList"
+import { useState } from "react"
 
 function App() {
-
-  return (
+  const [viewCart, setViewCart] = useState<boolean>(false)
+  const pageContent = viewCart ?  <Cart /> : <ProductList />
+  const content = (
     <>
-      <div className="App">
-        
-      </div>
+    <Header viewCart={viewCart} setViewCart={setViewCart}/>
+    {pageContent}
+    <Footer viewCart={viewCart} />
     </>
   )
+
+  return content
 }
 
 export default App
