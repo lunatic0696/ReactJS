768f764846092f5aab614d10801c8e32020bcadd
Alice Ferreira <alice.hex@proton.me>
2025-02-20 13:28:19 -0300

    Finished

--- shopping-cart/src/components/Product.tsx
+++ shopping-cart/src/components/Product.tsx
@@ -1,6 +1,6 @@
 import { ProductType } from "../context/ProductsProvider"
 import { ReducerActionType, ReducerAction } from "../context/CartProvider"
-import { ReactElement } from "react"
+import { ReactElement, memo } from "react"
 
 type PropsType = {
     product: ProductType,
@@ -15,18 +15,29 @@ const Product = ({ product, dispatch, REDUCER_ACTIONS, inCart } : PropsType): Re
 
     const onAddToCart = () => dispatch({ type: REDUCER_ACTIONS.ADD, payload: { ...product, qty: 1 }})
 
-    const itemInCart = inCart ? ' => Item in Cart: Yes' : null
+    const itemInCart = inCart ? ' -> Item in Cart: Yes' : null
 
     const content = 
         <article className="product">
             <h3>{product.name}</h3>
             <img src={img} alt={product.name}
             className="product__img" />
-            <p>{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'}).format(product.price)}</p>
+            <p>{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'}).format(product.price)}{itemInCart}</p>
             <button onClick={onAddToCart}>Add to Cart</button>
         </article>
 
     return content
 }
 
-export default Product
+function areProductsEqual(
+    { product: prevProduct, inCart:prevInCart }: PropsType, 
+    { product: nextProduct, inCart: nextInCart }: PropsType){
+        return(
+            Object.keys(prevProduct).every(key =>{
+                return prevProduct[key as keyof ProductType] ===nextProduct[key as keyof ProductType]
+            }) && prevInCart === nextInCart
+        )
+    }
+const MemoizedProduct = memo<typeof Product>(Product, areProductsEqual)
+
+export default MemoizedProduct
