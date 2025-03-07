398d49ba2bc1a6fc8eab003ab272727c8fa9a93d
Alice Ferreira <alice.hex@proton.me>
2025-03-07 13:34:50 -0300

    Header Enchancement

--- testing-site/src/components/Header.tsx
+++ testing-site/src/components/Header.tsx
@@ -1,52 +1,52 @@
-import { Navbar, Container, Nav, NavDropdown, Col, Row } from "react-bootstrap";
-
+import { Navbar, Nav, NavDropdown, Col, Row } from "react-bootstrap";
+import TransFlag from "./TransFlag";
+import "./Style.css"
 
 function Header() {
-    return(
-        <Navbar data-bs-theme="dark" expand="lg" className="bg-body-tertiary" fixed="top">
-      <Container>
-        <Navbar.Brand >
-          <Container>
-              <Row>
-                <Col>
-                  <img 
-                    src="src/assets/avatar.png"
-                    width="60"
-                    height="auto"
-                  />
-                </Col>
-                <Col>
-                  <Row>
-
-                  </Row>
-                  <Row>
-                    
-                  </Row>
-                </Col>
-              </Row>
-          </Container>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    )
+  return(
+  <Navbar expand="lg" className="bg-body-secondary" fixed="top" style={{padding: "0px"}}>
+      <Navbar.Brand style={{paddingLeft: "20px", paddingBottom: "0px", paddingTop: "0px"}}>
+        <Row>
+          <Col style={{padding: "10px"}}>
+            <img 
+              src="src/assets/avatar.png"
+              width="60"
+              height="auto"
+            />
+          </Col>
+          <Col>
+            <TransFlag/>
+          </Col>
+          <Col className="align-self-center">
+            <Row>
+              Alice
+            </Row>
+            <Row>
+              Ferreira
+            </Row>
+          </Col>
+        </Row>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Link</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+  </Navbar>
+  )
 }
 
 export default Header;
---
+++ testing-site/src/components/TransFlag.tsx
@@ -0,0 +1,13 @@
+function TransFlag() {
+    return(
+        <div className="transflag" style={{height: "100%"}}>
+            <div className="tf-blue" style={{backgroundColor: "#5BCEFA", width: "3px", height: "100%", float: "left"}}/>
+            <div className="tf-pink" style={{backgroundColor: "#F5A9B8", width: "3px", height: "100%", float: "left"}}/>
+            <div className="tf-white" style={{backgroundColor: "#FFFFFF", width: "3px", height: "100%", float: "left"}}/>
+            <div className="tf-pink" style={{backgroundColor: "#F5A9B8", width: "3px", height: "100%", float: "left"}}/>
+            <div className="tf-blue" style={{backgroundColor: "#5BCEFA", width: "3px", height: "100%", float: "left"}}/>
+        </div>
+    );
+}
+
+export default TransFlag;
